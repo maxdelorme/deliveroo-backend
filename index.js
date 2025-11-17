d1cc2dfb7c36f6da1906efde48578d67aee87819
Maxime Delorme <max@MacBook-Pro-de-Maxime.local>
2025-11-17 18:24:39 +0100

    with real json data

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+const axios = require("axios");
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const data = require("./assets/json/data.json");
 const app = express();
 app.use(cors());
 
-app.get("/", (req, res) => {
-  return res.json(data);
+app.get("/", async (req, res) => {
+  try {
+    const config = {
+      headers: { Authorization: `Bearer ${process.env.BEARER}` },
+    };
+    const response = await axios.get(
+      "https://lereacteur-bootcamp-api.herokuapp.com/api/deliveroo/menu/Paris/paris-3e-temple/hank-burger-paris-archives?geohash=u09wjb16vbhd",
+      config
+    );
+    res.json(response.data);
+  } catch (error) {}
 });
 
 // Northflank va nous fournir une variable process.env.PORT
